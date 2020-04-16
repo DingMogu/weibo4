9e0e4877bfcc0f4b4bac4517128b1a634f999432
rui <1975261197@qq.com>
2020-04-16 07:29:15 +0000

    增加csrg验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,14 +7,30 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    /*
+     * 创建
+     */
     public function create()
     {
         return view('users.create');
     }
 
+    /*
+     * 个人信息
+     * */
     public function show(User $user)
     {
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
+    }
+    /*
+     * 创建验证
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
     }
 }
