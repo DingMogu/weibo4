43be246be69bcb695c26fa7fbfaf5c873f4fe6bf
rui <1975261197@qq.com>
2020-04-18 06:43:19 +0000

    创建会话

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class SessionsController extends Controller
 {
@@ -10,5 +11,24 @@ class SessionsController extends Controller
     public function create()
     {
         return view('sessions.create');
+    }
+
+    /*
+     * 登录功能验证
+     */
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request,[
+           'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials)){
+            session()->flash('success', '欢迎回来! ');
+            return redirect()->route('users.show',[Auth::user()]);
+        }else{
+            session()->flash('danger', ' 很抱歉,您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
     }
 }
