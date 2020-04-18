8c76a5d8ec393c7885efa57a4d16d335d3a7ccd7
rui <1975261197@qq.com>
2020-04-18 07:37:27 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -18,17 +18,27 @@ class SessionsController extends Controller
      */
     public function store(Request $request)
     {
-        $credentials = $this->validate($request,[
-           'email' => 'required|email|max:255',
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials)){
+        if (Auth::attempt($credentials)) {
             session()->flash('success', '欢迎回来! ');
-            return redirect()->route('users.show',[Auth::user()]);
-        }else{
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
             session()->flash('danger', ' 很抱歉,您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
+    }
+
+    /*
+     * 退出当前用户
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出');
+        return redirect('login');
     }
 }
