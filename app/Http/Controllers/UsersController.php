916caca4a018a77326654aeaad2c295968c39316
rui <1975261197@qq.com>
2020-05-24 05:55:38 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-           'except' => ['show', 'create', 'store']
+           'except' => ['show', 'create', 'store','index']
         ]);
 
         $this->middleware('guest', [
@@ -94,5 +94,15 @@ class UsersController extends Controller
     {
         $users = User::paginate(6);
         return view('users.index',compact('users'));
+    }
+    /**
+     * 删除用户
+     */
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy',$user);
+        $user->delete();
+        session()->flash('success','成功删除用户');
+        return back();
     }
 }
